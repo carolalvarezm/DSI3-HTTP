90b613b697868bce7a824370de3f5f78696169c3
Carolina Álvarez Martín <alu0100944723@ull.edu.es>
2020-04-09 19:07:34 +0100

    Añadiendo GET a los métodos

--- src/server.js
+++ src/server.js
@@ -24,3 +24,22 @@ function urlToPath(url) {
     var path = require("url").parse(url).pathname;
     return "." + decodeURIComponent(path);
 }
+
+methods.GET = function(path, respond) {
+    fs.stat(path, function(error, stats) {
+        if (error && error.code == "ENOENT")
+            respond(404, "File not found");
+        else if (error)
+            respond(500, error.toString());
+        else if (stats.isDirectory())
+            fs.readdir(path, function(error, files) {
+                if (error)
+                    respond(500, error.toString());
+                else
+                    respond(200, files.join("\n"));
+            });
+        else
+            respond(200, fs.createReadStream(path),
+                require("mime").lookup(path));
+    });
+};
